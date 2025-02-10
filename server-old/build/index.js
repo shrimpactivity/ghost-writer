6b87d48012f69b143d7d208024cf3455806c2eba
Carson Crow <carsoncrow@proton.me>
2025-02-10 14:37:58 -0600

    move prettier + eslint config

--- server-old/build/index.js
+++
@@ -1,6 +0,0 @@
-const http = require("http");
-const app = require("./app");
-const PORT = process.env.PORT || 3005;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
--- server-old/build/middleware/requestLogger.js
+++
@@ -1,9 +0,0 @@
-const requestLogger = (request, response, next) => {
-    console.info("");
-    console.info("=== Begin Request ===");
-    console.info("Method: ", request.method);
-    console.info("Path: ", request.path);
-    console.info("=====================");
-    next();
-};
-module.exports = requestLogger;
